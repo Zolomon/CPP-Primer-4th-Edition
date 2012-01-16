8d7298126df51ca24100b6c7593e3291e30a391f
Bengt Ericsson <Zolomon@gmail.com>
2012-01-16 12:48:26 +0100

    Finished 1.4

---
+++ src/01/04.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+  int v1, v2;
+  
+  std::cout << "Enter two numbers:" << std::endl;
+  std::cin >> v1 >> v2;
+  std::cout << "The product of " << v1 << " and " << v2 << 
+    " is " << v1 * v2 << "." << std::endl;
+    
+  return 0;
+}
