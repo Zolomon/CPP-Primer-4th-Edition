96a8a777bc58a2f67d4edaab51c8fab85e684ec0
Bengt Ericsson <Zolomon@gmail.com>
2012-01-23 15:57:51 +0100

    Finished 2.01.

---
+++ src/02/01.cc
@@ -0,0 +1,12 @@
+/*
+		Size:									Bits:
+int: 	signed: -2147483648 to 2147483647 		One word (32 bits) on the computer it is compiled one, usually 32 bits
+		unsigned: 0 to 4294967295
+
+long:	signed: -2147483648 to 2147483647		One word (32 bits)
+		unsigned: 0 to 4294967295
+
+short:	signed: -32768 to 32767					One chunk (16 bits)
+		unsigned: 0 to 65535
+
+*/
