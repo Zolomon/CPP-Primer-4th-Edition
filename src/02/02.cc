534f4606cf487345edcc6b9df9bb9fa977dcfc73
Bengt Ericsson <Zolomon@gmail.com>
2012-01-23 15:58:17 +0100

    Finished 2.02.

---
+++ src/02/02.cc
@@ -0,0 +1,9 @@
+/*
+
+Unsigned uses all bits of the representation to produce the values from 0 to the max value possible for that amount of bits. 
+Example value: 0 - 255.
+
+Signed uses one bit for sign or 0. When an object is signed one bit of value precision will be lost. 
+Example value: -128 to 127.
+
+*/
