4d876e6337b0abb6297471fa9fbdda96214ea33e
Bengt Ericsson <Zolomon@gmail.com>
2012-01-23 15:18:08 +0100

    Finished 1.26.

---
+++ src/01/26.cc
@@ -0,0 +1,4 @@
+// In a former section of the book, page 21-22, it states the operations we 
+// know about the Sales_item class. The compound assignment operator isn't 
+// stated in there so we don't know that we can use it (even if it's been 
+// implemented in the header file as an inline function)
