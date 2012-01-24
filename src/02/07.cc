690f69302531dbe9a16710161c666aafdd60013c
Bengt Ericsson <Zolomon@gmail.com>
2012-01-24 12:04:10 +0100

    Finished 2.07.

---
+++ src/02/07.cc
@@ -0,0 +1,29 @@
+/*
+
+ 'a'  = Character literal, 8 bits, represents single character.
+
+L'a'  = Wide character literal, 16 bits, represents single character.
+
+ "a"  = String literal, array of character literals + '\0' in last index.
+
+L"a"  = Wide string literal, array of w_char character literals + \0 in last index.
+
+10    = Integer value 10
+
+10u   = Unsigned integer value 10
+ 
+10L   = Long integer value 10
+ 
+10uL  = Unsigned long integer value 10
+ 
+012   = 10 in octadecimal representation
+ 
+0xC   = 10 in hexadecimal representation
+ 
+3.14  = Double value 3.14.
+ 
+3.14f = Float value 3.14, single precision.
+ 
+3.14L = Long value 3.14, extended precision.
+ 
+*/
