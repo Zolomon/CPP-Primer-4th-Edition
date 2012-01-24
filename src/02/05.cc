76955bffbe08b1cce54c8277f6b4244fab9efdef
Bengt Ericsson <Zolomon@gmail.com>
2012-01-24 11:38:33 +0100

    Finished 2.05.

---
+++ src/02/05.cc
@@ -0,0 +1,12 @@
+/*
+
+Floats are represented in 32 bits (one word) and 
+doubles in 64 bits (two words). 
+
+Floats represent 6 significant digits while a double represent 
+10 significant digits. This means that floats aren't very accurate and 
+lose precision in calculations when compared to doubles. 
+
+It's almost always right to use double instead of float.
+
+*/
