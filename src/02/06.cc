bcaea8b46f0d7b418902acc1ff08147786b0ff60
Bengt Ericsson <Zolomon@gmail.com>
2012-01-24 11:50:23 +0100

    Finished 2.06.

---
+++ src/02/06.cc
@@ -0,0 +1,8 @@
+/*
+
+I would use double for all, or even implement my own currency class that can handle precision better than double. With banker's rounding and all... 
+
+
+Anyway; since we're talking about money, you need the precision given by double, that's why you shouldn't use float in this case, or integer. 
+
+*/
