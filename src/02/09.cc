70b009875eadcdad09bae9cbf0f5bc116c1d4b32
Bengt Ericsson <Zolomon@gmail.com>
2012-01-24 12:15:18 +0100

    Finished 2.09.

---
+++ src/02/09.cc
@@ -0,0 +1,11 @@
+/*
+
+(a) "Who goes with F\145rgus?\012" = Legal
+(b) 3.14e1L = legal
+(c) "two" L"some" = illegal/undefined
+(d) 1024f = legal
+(e) 3.14UL = illegal
+(f) "multiple line
+     comment" = illegal, need to end line with \
+
+*/
