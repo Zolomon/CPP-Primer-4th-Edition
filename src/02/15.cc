ffde6ac9771fab27e1a02ff119ee5b608dc9bfcc
Bengt Ericsson <Zolomon@gmail.com>
2012-02-07 18:29:49 +0100

    Finished 2.15.

---
+++ src/02/15.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int month = 9, day = 7;
+	cout << month << " " << day << endl;
+
+	// Error: 0 before a digit means it's using octal representation to specify the value.
+	// This means that you can't use 09 because 9 > 8.
+	//month = 09, day = 07;
+	//cout << month << " " << day << endl;
+
+	return 0;
+}
