cd823d55f39a18ffca4b8b6c553047b7465bf627
Bengt Ericsson <Zolomon@gmail.com>
2012-02-07 18:34:40 +0100

    Finished 2.16.

--- src/02/15.cc
+++ src/02/15.cc
@@ -7,6 +7,7 @@ int main() {
 	int month = 9, day = 7;
 	cout << month << " " << day << endl;
 
+	// Commented lines are errors.
 	// Error: 0 before a digit means it's using octal representation to specify the value.
 	// This means that you can't use 09 because 9 > 8.
 	//month = 09, day = 07;
---
+++ src/02/16.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+double calc() {
+	
+	return 0.0;
+}
+
+int main() {
+	// Commented lines are errors.
+	//int car = 1024, auto = 2048; // auto is keyword
+	int ival = ival;
+	//std::cin >> int input_value; // input_value needs to be declared before use.
+	//double salary = wage = 9999.99; // wage needs to be declared before use.
+	//double calc = calc();
+
+	std::cout << ival << std::endl;
+
+	return 0;
+}
