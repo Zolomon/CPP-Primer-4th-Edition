6b012e07bda70192e1564904ea3e7cd97634b114
Bengt Ericsson <Zolomon@gmail.com>
2012-02-07 16:43:17 +0100

    Finished 2.12.

---
+++ src/02/12.cc
@@ -0,0 +1,10 @@
+/*
+
+lvalue = Expressions that may appear as either the left-hand or right-hand side of an assignment. Have an address.
+
+rvalue = Expressions that may appear on the right-hand but not left-hand side of an assignment. Does not have an address.
+
+Examples: 
+int units_sold = 0; // units_sold = lvalue, 0 = rvalue
+
+*/
