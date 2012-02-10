51b7825aa0da3dc92b1beeaa8f327dedbc0e1418
Bengt Ericsson <Zolomon@gmail.com>
2012-02-10 21:55:57 +0100

    Finished 3.02.

---
+++ src/03/02.cc
@@ -0,0 +1,15 @@
+/*
+
+A default constructor is the constructor that is used by default when no parameters are specified. 
+
+For example: 
+
+class Example
+{
+public:
+	Example();
+};
+
+Here, We can instantiate our type on the stack with Example myExample; and it will be equivalent to Example myExample();.
+
+*/
