7ca32d14f5240e177134083ada173453662658bf
Bengt Ericsson <Zolomon@gmail.com>
2012-02-10 22:36:19 +0100

    Finished 3.04.c

---
+++ src/03/04.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s; // "" - empty string
+int main() 
+{
+	string s2; // "" - empty string
+
+	cout << "s[" << s << "], s2[" << s2 << "]" << endl;
+	return 0;
+}
