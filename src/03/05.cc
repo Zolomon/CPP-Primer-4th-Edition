9a0406d4dc43dd8f4bba137e5f78f9b274fb06f5
Bengt Ericsson <Zolomon@gmail.com>
2012-02-10 22:36:28 +0100

    Finished 3.05.

---
+++ src/03/05.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	//string line;
+	// while (getline(cin, line))
+	// 	cout << line << endl;
+
+	string word;
+	while (cin >> word)
+		cout << word << endl;
+
+	return 0;
+}
