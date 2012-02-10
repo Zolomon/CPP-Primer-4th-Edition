80773e8d40db987b71713fbb1478a1fbd2de309c
Bengt Ericsson <Zolomon@gmail.com>
2012-02-10 22:38:47 +0100

    Finished 3.06.

---
+++ src/03/06.cc
@@ -0,0 +1,11 @@
+/*
+
+input operator for strings: 
+Leading whitespace is discarded. 
+Then everything is read until next whitespace occurs, whitespace non-including.
+
+getline: 
+Leading new lines are not discarded. 
+Then everything is read until next newline character, newline character non-including.
+
+*/
